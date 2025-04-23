c650d255c68adb875cf2f47c88377d12a27ada75
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-04-23 13:27:37 -0700

    Using datetime2 and having Visual Studio let SSMS use it's getutcdate property, allowing accurate dates to be made.

--- Flutter Score Project/Models/Score.cs
+++ Flutter Score Project/Models/Score.cs
@@ -1,9 +1,13 @@
-﻿namespace Flutter_Score_Project.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Flutter_Score_Project.Models
 {
     public class Score
     {
         public long ScoreId { get; set; }
         public int ScoreResult { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime DateCreated { get; set; }
     }
 }
