a6707f93c9a3bee036aaa690ebed8ed5f65b0471
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-05-09 14:05:14 -0700

    Time to begin working on final project. Endpoints and changes made so that we can have Login and Register

---
+++ Flutter Score Project/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Flutter_Score_Project.Data;
+using Flutter_Score_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flutter_Score_Project.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public IActionResult Register(string UserName, string Password, string Email)
+        {
+            ScoreContext _context = new ScoreContext();
+            if (_context.user.Any(u => u.UserName == UserName))
+            {
+                return BadRequest(new { message = "Username already exists!" });
+            }
+            if (_context.user.Any(u => u.Email == Email))
+            {
+                return BadRequest(new { message = "Email already exists!" });
+            }
+            var newUser = new User
+            {
+                UserName = UserName,
+                Password = Password,
+                Email = Email
+            };
+            _context.user.Add(newUser);
+            _context.SaveChanges();
+            return Ok(new { message = "User registered successfully!" });
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(string UserName, string Password)
+        {
+            ScoreContext _context = new ScoreContext();
+            var user = _context.user.FirstOrDefault(u => u.UserName == UserName && u.Password == Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Invalid username or password!" });
+            }
+            return Ok(new { message = "Login successful!", userId = user.UserId });
+        }
+    }
+}
--- Flutter Score Project/Models/User.cs
+++ Flutter Score Project/Models/User.cs
@@ -7,6 +7,8 @@ namespace Flutter_Score_Project.Models
     {
         public long UserId { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         [JsonIgnore] // prevents endless loop cycle between User and Score callbacks
         public ICollection<Score> Scores { get; set; }
     }
